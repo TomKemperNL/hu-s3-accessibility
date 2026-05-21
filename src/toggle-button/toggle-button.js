ebc871ecaf062a591f95692fba6a416e96e67041
Tom <tom.kemper@hu.nl>
2026-05-21 21:10:14 +0200

    Ok, label for is alleen voor form-meuk

--- src/toggle-button/toggle-button.js
+++ src/toggle-button/toggle-button.js
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from "lit";
 
 export class ToggleButton extends LitElement {
-
+    static formAssociated = true;
+    
     static styles = css`
-        div {
+        :host {
             cursor: default;
             display: inline-block;
             border: 1px solid #ccc;
@@ -15,7 +16,7 @@ export class ToggleButton extends LitElement {
             }
         }
 
-        div.pressed {
+        :host(.pressed) {
             background-color: #00ff08;
             color: #000000;
 
@@ -32,14 +33,29 @@ export class ToggleButton extends LitElement {
     constructor() {
         super();
         this.pressed = false;
+        this.role = 'button';
+        this.tabIndex = 0;
+        this.internals = this.attachInternals();
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.addEventListener('click', () => {
+            this.pressed = !this.pressed;
+        });
+        this.addEventListener('keydown', (e) => {
+            if (e.key === ' ' || e.key === 'Enter') {
+                e.preventDefault();
+                this.pressed = !this.pressed;
+            }
+        });
     }
 
     render() {
-        return html`
-            <div tabindex="0" role="button" aria-pressed="${this.pressed}"
-                @click="${() => this.pressed = !this.pressed}" class="${this.pressed ? 'pressed' : ''}">
+        this.classList.toggle('pressed', this.pressed);
+        this.ariaPressed = this.pressed ? 'true' : 'false';
+        return html`           
                 ${this.pressed ? html`<span>ON</span>` : html`<span>OFF</span>`}
-            </div>
         `;
     }
 }
